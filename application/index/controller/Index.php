7466cd83b7b4c1bb3b8f092c4878d9df76c55d8d
mskies <3792535@qq.com>
2016-11-07 09:22:10 +0800

    step2.5

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -1,10 +1,13 @@
 <?php
+// 该文件位于application\index\controller文件夹中
 namespace app\index\controller;
 
+// 我的文件名叫做Index.php，我的名字对应的叫做Index。
 class Index
 {
+    // 我的方法名叫index
     public function index()
     {
-        return 'Hello World';
+        return 'Hello World!';
     }
 }
