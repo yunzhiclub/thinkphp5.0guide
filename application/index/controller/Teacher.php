ad0f22f7dfe54a276dc95b00afd6ee0fd0f8521b
mskies <3792535@qq.com>
2016-11-07 10:26:58 +0800

    step3.2.4.2

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\index\controller;     // 该文件的位于application\index\controller文件夹
-use app\common\model\Teacher;       // 教师模型
+// 导入app\common\model\Teacher模型，并给它重新起个名字：SmallTeacher
+use app\common\model\Teacher as SmallTeacher;  // 教师模型 带有别名
 /**
  * 教师管理
  */
@@ -8,7 +9,7 @@ class Teacher
 {
     public function index()
     {
-        $Teacher = new Teacher;
-        dump($Teacher);
+        $SmallTeacher = new SmallTeacher;
+        dump($SmallTeacher);
     }
 }
