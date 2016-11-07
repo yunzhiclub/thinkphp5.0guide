d9e4524b8c8210dd2e114d6b46bcbfbe606ddcae
mskies <3792535@qq.com>
2016-11-07 14:19:53 +0800

    step3.5.6

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -136,7 +136,23 @@ class TeacherController extends Controller
      */
     public function update()
     {
-        var_dump(Request::instance()->post());
+        // 接收数据
+        $teacher = Request::instance()->post();
+
+        // 将数据存入Teacher表
+        $Teacher = new Teacher();
+        $message = '更新成功';
+
+        // 依据状态定制提示信息
+        try {
+            if (false === $Teacher->validate(true)->isUpdate()->save($teacher)) {
+                $message = '更新失败：' . $Teacher->getError();
+            }
+        } catch (\Exception $e) {
+            $message = '更新失败:' . $e->getMessage();
+        }
+
+        return $message;
     }
 }
 
