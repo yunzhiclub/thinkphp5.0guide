add0f09922e9086e421efac06ad778e08d991099
mskies <3792535@qq.com>
2016-11-08 07:45:19 +0800

    修正删除的BUG

--- application/index/controller/TeacherController.php
+++ application/index/controller/TeacherController.php
@@ -128,7 +128,7 @@ class TeacherController extends Controller
             $id = Request::instance()->param('id/d');
             
             // 判断是否成功接收
-            if (0 === $id) {
+            if (is_null($id) || 0 === $id) {
                 throw new \Exception('未获取到ID信息', 1);
             }
 
