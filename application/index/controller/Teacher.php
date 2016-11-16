cca487a2b53082a4350e105ed76c15b8473a1eb9
mskies <3792535@qq.com>
2016-11-16 09:16:13 +0800

    step3.2.3.3

--- application/index/controller/Teacher.php
+++ application/index/controller/Teacher.php
@@ -7,9 +7,12 @@ class Teacher
     public function index()
     {
         // 获取教师表中的所有数据
-        $teachers = Db::name('teacher')->select();
+        $teachers = DB::name('teacher')->select();
+
+        // 用下面的语句，也可以直接返回给用户
+        echo $teachers[0]['name'];
 
         // 查看获取的数据
-        var_dump($teachers);
+        return $teachers[0]['name'];
     }
 }
